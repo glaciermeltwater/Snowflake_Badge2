001fdf6e07ed78ffa9694c076ad4594ff9644943
Glacier Meltwater <112290594+glaciermeltwater@users.noreply.github.com>
2022-08-27 19:52:50 -0600

    Update Snowflake_Badge2_file.py

--- Snowflake_Badge2_file.py
+++ Snowflake_Badge2_file.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('My Parents New Healthy Diner')
-streamlit.body('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
