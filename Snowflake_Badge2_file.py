3d716ca0703770a83fc9024c467ec2aecdd410cf
Glacier Meltwater <112290594+glaciermeltwater@users.noreply.github.com>
2022-09-10 21:35:31 -0600

    Update Snowflake_Badge2_file.py

--- Snowflake_Badge2_file.py
+++ Snowflake_Badge2_file.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorite')
@@ -7,10 +10,8 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
    
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
@@ -26,15 +27,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write the responded selection data
 streamlit.dataframe(fruityvice_normalized) 
+streamlit.stop()
 
-import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
